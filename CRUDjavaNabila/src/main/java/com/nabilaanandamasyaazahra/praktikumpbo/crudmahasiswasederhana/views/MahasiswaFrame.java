67e56deea0c56ace90de39f106ee39f58efbb230
nabilaaaaaaaaaaa <93029439+nabilaaaaaaaaaaa@users.noreply.github.com>
2022-12-27 11:33:17 +0700

    Add files via upload

---
+++ CRUDjavaNabila/src/main/java/com/nabilaanandamasyaazahra/praktikumpbo/crudmahasiswasederhana/views/MahasiswaFrame.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.nabilaanandamasyaazahra.praktikumpbo.crudmahasiswasederhana.views;
+
+/**
+ *
+ * @author HP
+ */
+public class MahasiswaFrame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
